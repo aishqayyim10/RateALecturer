efe2a56ff2b8971026c65ce7037e0e337d5c9e98
aish <aishqayyim@gmail.com>
2026-03-06 19:25:43 +0800

    staticfiles

--- config/config/settings.py
+++ config/config/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Redirect to the home page after logging in
 LOGIN_REDIRECT_URL = 'home'
